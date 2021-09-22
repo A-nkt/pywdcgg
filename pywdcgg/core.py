772ba828aade5c9f679244560f771f84326e9383
YuUuTta <dsduoa31@gmail.com>
2021-09-22 16:25:47 +0900

    make file

---
+++ pywdcgg/core.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def read_file(dir_file):
+    """
+    read data from text file that download from WDCGG.
+
+    Parameters
+    ----------
+    dir_file : str
+        file path and your file name
+
+    Returns
+    ----------
+    df : pd.DataFrame
+        output file
+
+    """
+    pass
