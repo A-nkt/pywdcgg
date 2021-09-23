2c95c5f70e7f9438a46b68fe8192799f8791e547
YuUuTta <dsduoa31@gmail.com>
2021-09-23 22:51:09 +0900

    developing

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ this repository can be use for analysis of [WDCGG](https://gaw.kishou.go.jp/jp) 
 ## install
 
 ## Requirement
+- pandas
 
 ## Quickstart
 
 ## Note
 if you want to new function related to WDCGG.
 please contact me. off course please pull request.
+
+---
--- pywdcgg/pywdcgg.py
+++ pywdcgg/pywdcgg.py
@@ -16,6 +16,24 @@ class read_file():
             output file
 
         """
-        print("Hello World.")
+        with open(dir_file) as f:
+            data_get = False
+            l_strip = [s.strip() for s in f.readlines()]
+            for indx,l in enumerate(l_strip):
+                if l[0] != "#":
+                    header_row = l_strip[indx-1]
+                    skiprows = indx
+                    break
+
+        header_list = header_row.split(" ")[1:]
+        df = pd.read_csv(dir_file,skiprows=skiprows,header=None,sep=" ")
+        df.columns = header_list
+        print(df)
     def Info(self):
+        """
+        get about file info
+        """
         pass
+
+
+# linear interpolation about lat,lon,height
