94d69a9e4094675cc824956710afd51d320d81df
YuUuTta <dsduoa31@gmail.com>
2021-09-24 00:09:36 +0900

    main

--- examples/tutorials/read_file.py
+++ examples/tutorials/read_file.py
@@ -2,8 +2,8 @@ import sys
 sys.path.append("../../pywdcgg/")
 import pywdcgg as pw
 
+# test data
 # hfc134a : ../data/hfc134a/MHD/monthly/hfc134a_mhd_surface-insitu_4_2023-2022_monthly.txt
-# hfc134a : ../data/hfc134a/MHD/event/hfc134a_mhd_surface-insitu_4_2023-2022_monthly.txt
 # ch4 : ../data/ch4/SYO/monthly/ch4_syo_surface-flask_2_3001-9999_monthly.txt
 # ch4 : ../data/ch4/SYO/event/ch4_syo_surface-flask_2_3001-9999_event.txt
 rdat = pw.read_file("../data/ch4/SYO/event/ch4_syo_surface-flask_2_3001-9999_event.txt")
